7267e7ed6c758d587e2b0414c3cca47090eccd96
DrRungo <DrRungo@users.noreply.github.com>
2019-11-29 15:57:05 +0100

    More SQL

--- src/sample/data/SQLMediaMapper.java
+++ src/sample/data/SQLMediaMapper.java
@@ -1,4 +1,35 @@
 package sample.data;
 
-public class SQLMediaMapper {
+import sample.logic.entities.Media;
+import sample.logic.interfaces.MediaMapper;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLMediaMapper implements MediaMapper {
+    @Override
+    public List<Media> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Media> getAllMovies() {
+        return null;
+    }
+
+    @Override
+    public List<Media> getAllSeries() {
+        return null;
+    }
+
+    @Override
+    public List<Media> getAllFromCategory(String category) {
+        return null;
+    }
+
+    @Override
+    public Media get(int id) {
+        return null;
+    }
 }
