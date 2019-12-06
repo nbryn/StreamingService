0c7b20f4f5b1c09aefcb3d7bde756d0fe7755257
DrRungo <DrRungo@users.noreply.github.com>
2019-12-06 14:30:18 +0100

    Fixed parsing errors

--- src/sample/data/SQLMediaMapper.java
+++ src/sample/data/SQLMediaMapper.java
@@ -39,22 +39,22 @@ public class SQLMediaMapper implements MediaMapper {
 
     @Override
     public List<Media> getByName(String name, String media){
-        return sendQuery("SELECT * FROM movies WHERE 'name'  = " + name, "SELECT * FROM series WHERE 'name' = " +name,media);
+        return sendQuery("SELECT * FROM movies WHERE name  = " + name, "SELECT * FROM series WHERE name = " +name,media);
     }
 
     @Override
     public List<Media> getByRating(double rating, String media){
-        return sendQuery("SELECT * FROM movies WHERE 'rating' >= " + rating,"SELECT * FROM series WHERE 'rating' >=" + rating, media);
+        return sendQuery("SELECT * FROM movies WHERE rating >= " + rating,"SELECT * FROM series WHERE rating >=" + rating, media);
     }
 
     @Override
     public List<Media> getAllFromGenre(String genre, String media){
-        return sendQuery("SELECT * FROM movies WHERE 'genre' LIKE '%" + genre +"%'","SELECT * FROM series WHERE 'genre' LIKE '%" + genre +"%'", media);
+        return sendQuery("SELECT * FROM movies WHERE genre LIKE '%" + genre +"%'","SELECT * FROM series WHERE genre LIKE '%" + genre +"%'", media);
     }
 
     @Override
     public List<Media> getByRelease(int year, String media){
-        return sendQuery("SELECT * FROM movies WHERE 'release' >= " + year,"SELECT * FROM series WHERE 'release' >= " + year, media);
+        return sendQuery("SELECT * FROM movies WHERE release >= " + year,"SELECT * FROM series WHERE release >= " + year, media);
     }
     private List<Media> sendQuery(String movieQuery, String seriesQuery, String media){
         List<Media> allMovies = null;
