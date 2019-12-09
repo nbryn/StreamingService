02a567a1a9c195d54cccbd56567d8d0c9abe7301
nbryn <niklasbg@live.dk>
2019-12-09 15:30:48 +0100

    Implemented logic for sortByRating and show all movies/series

--- src/sample/OverviewController.java
+++ src/sample/OverviewController.java
@@ -10,6 +10,9 @@ import sample.data.SQLMediaMapper;
 import sample.data.SQLUserMapper;
 import sample.logic.AppController;
 import sample.logic.entities.Media;
+import sample.logic.entities.Movie;
+import sample.logic.entities.Series;
+import sample.logic.interfaces.MediaMapper;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -17,12 +20,15 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OverviewController {
 
     private AppController appController;
 
-    List<File> fileList;
+    private List<Media> allMedia;
+
+    private List<File> fileList;
 
     @FXML
     private GridPane gridPane;
@@ -39,6 +45,7 @@ public class OverviewController {
     public OverviewController() {
         appController = new AppController(new SQLUserMapper(), new SQLMediaMapper());
         fileList = new ArrayList<>();
+        allMedia = appController.fetchAll("all");
     }
 
     public void updateView(List<Media> mediaList) {
@@ -56,42 +63,13 @@ public class OverviewController {
         File[] seriesImg = new File(seriesPath).listFiles();
         File[] moviesImg = new File(moviePath).listFiles();
 
-
         List<File> images = new ArrayList<>(Arrays.asList(seriesImg));
         Collections.addAll(images, moviesImg);
 
-        for (File file : images) {
-            for (Media media : mediaList) {
-                if (file.getName().equals(media.getName() + ".jpg")) {
-                    fileList.add(new File(file.toURI().toString()));
-                }
-            }
-        }
-        int rows = (fileList.size() / 4) + 1;
-        int columns = 4;
-        int index = 0;
+        addToFileList(images, mediaList);
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                if (index < fileList.size()) {
-                    addImage(index, j, i);
-                    index++;
-                }
-            }
-        }
-    }
+        generateView();
 
-    public void addImage(int index, int row, int column) {
-        Image img = new Image(String.valueOf(fileList.get(index)));
-
-        ImageView imgView = new ImageView(img);
-        imgView.setFitWidth(175);
-        imgView.setFitHeight(250);
-
-        gridPane.setHgap(5);
-        gridPane.setVgap(5);
-        GridPane.setConstraints(imgView, column, row);
-        gridPane.getChildren().add(imgView);
     }
 
 
@@ -118,8 +96,6 @@ public class OverviewController {
     public void search(ActionEvent event) {
         String searchString = searchTextField.getText().trim();
 
-        System.out.println(searchString);
-
         List<Media> result = appController.fetchByName(searchString, "all");
 
         updateView(result);
@@ -127,36 +103,52 @@ public class OverviewController {
     }
 
     @FXML
-    public void loadAllMedia(ActionEvent event) {
-        List<Media> medias = appController.fetchAll("all");
-
-        updateView(medias);
+    public void showAll(ActionEvent event) {
+        updateView(allMedia);
 
     }
 
     @FXML
-    public void loadAllSeries(ActionEvent event) {
-        List<Media> series = appController.fetchAll("series");
+    public void showAllSeries(ActionEvent event) {
+        List<Media> series = allMedia
+                .stream()
+                .filter(media -> media instanceof Series)
+                .collect(Collectors.toList());
+
+        updateView(series);
 
     }
 
     @FXML
-    public void loadAllMovies(ActionEvent event) {
-        List<Media> movies = appController.fetchAll("movies");
+    public void showAllMovies(ActionEvent event) {
+        List<Media> movies = allMedia
+                .stream()
+                .filter(media -> media instanceof Movie)
+                .collect(Collectors.toList());
+
+        updateView(movies);
+    }
+
+    @FXML
+    public void sortByRating(ActionEvent event) {
+        allMedia.sort((m1, m2) -> {
+            if (m1.getRating() > m2.getRating()) {
+                return 1;
+            } else if (m1.getRating() < m2.getRating()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
 
     }
 
     @FXML
-    public void loadRatingOver8(ActionEvent event) {
-        List<Media> result = appController.fetchRatingOver(8.00, "all");
+    public void sortByRelease(ActionEvent event) {
+        List<Media> result = appController.fetchRatingOver(7.00, "all");
 
     }
 
-    @FXML
-    public void loadRatingOver5(ActionEvent event) {
-        List<Media> result = appController.fetchRatingOver(5.00, "all");
-
-    }
 
     @FXML
     public void releaseAfter2000(ActionEvent event) {
@@ -195,8 +187,6 @@ public class OverviewController {
     public void loadDocumentary(ActionEvent event) {
         List<Media> result = appController.fetchAllFromGenre("Documentary", "all");
 
-        System.out.println(result);
-
         updateView(result);
     }
 
@@ -211,7 +201,6 @@ public class OverviewController {
     @FXML
     public void loadAction(ActionEvent event) {
         List<Media> result = appController.fetchAllFromGenre("Action", "all");
-
 
         updateView(result);
     }
@@ -228,5 +217,45 @@ public class OverviewController {
         List<Media> result = appController.fetchAllFromGenre("Drama", "all");
 
         updateView(result);
+    }
+
+    private void addToFileList(List<File> images, List<Media> mediaList) {
+        for (File file : images) {
+            for (Media media : mediaList) {
+                if (file.getName().equals(media.getName() + ".jpg")) {
+                    fileList.add(new File(file.toURI().toString()));
+                    mediaList.remove(media);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void generateView() {
+        int rows = (fileList.size() / 4) + 1;
+        int columns = 4;
+        int index = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (index < fileList.size()) {
+                    addImage(index, j, i);
+                    index++;
+                }
+            }
+        }
+    }
+
+    private void addImage(int index, int row, int column) {
+        Image img = new Image(String.valueOf(fileList.get(index)));
+
+        ImageView imgView = new ImageView(img);
+        imgView.setFitWidth(175);
+        imgView.setFitHeight(250);
+
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+        GridPane.setConstraints(imgView, column, row);
+        gridPane.getChildren().add(imgView);
     }
 }
